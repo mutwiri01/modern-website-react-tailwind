18722094284321bf3624211293a4d42cf979007c
mutwiri01 <mutwirikithinji01@gmail.com>
2024-04-01 23:37:36 +0300

    222

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import Banner1 from "../../assets/c1.png";
 
 const Banner = () => {
@@ -12,9 +13,12 @@ const Banner = () => {
             Together, we can make a positive impact on our communities and the planet. 
             Join us in our mission to create a cleaner, greener future for generations to come
             </p>
-            <button className="rounded-md bg-white px-4 py-2 text-sm  text-black transition-colors duration-300 hover:bg-white/90">
-              contact us
-            </button>
+            <Link to = '/about'>
+              <button className="rounded-md bg-white px-4 py-2 text-sm  text-black transition-colors duration-300 hover:bg-white/90">
+                contact us
+              </button>
+            </Link>
+            
           </div>
           <div className="order-1">
           <img src={Banner1} alt="No image" className="hover:drop-shadow-md" />
